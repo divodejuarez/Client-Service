75089bb4d125818a43e013b8044273d1992b8d3b
rodrigolopezag <rodrigolopezag@gmail.com>
2019-10-11 01:43:59 -0700

    Skeleton for whole platform and context to the firebase project

---
+++ client-service/src/constants/routes.js
@@ -0,0 +1,8 @@
+export const LANDING = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const PASSWORD_FORGET = '/psswd-forget';
+export const SUBSCRIBE = '/subscribe';
+export const ADMIN_ROUTINES = '/admin-routines';
+export const CHECK_ROUTINES = '/check-routines';
+export const LOG_ROUTINES = '/log-routines';
