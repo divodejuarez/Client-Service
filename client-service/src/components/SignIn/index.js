54160cdd7f1d9cf0db723e70875b829c8c248e55
rodrigolopezag <rodrigolopezag@gmail.com>
2019-10-16 01:42:04 -0700

    Added implementation of the sign in functionality

--- client-service/src/components/SignIn/index.js
+++ client-service/src/components/SignIn/index.js
@@ -1,35 +1,73 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
+import { SignUpLink } from '../SignUp';
+import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 const SignIn = () => (
   <div>
     <h1>Sign In</h1>
     <SignInForm />
+    <SignUpLink />
   </div>
 );
 
-class SignInForm extends Component {
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+  error: null
+};
+
+class SignInFormBase extends Component {
   constructor(props) {
     super(props);
+
+    this.state = { ...INITIAL_STATE };
   }
 
   onSubmit = event => {
+    const { email, password } = this.state;
 
+    this.props.firebase
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+
+      event.preventDefault();
   }
   
   onChange = event => {
-
+    this.setState({ [event.target.name]: event.target.value });
   };
 
   render() {
+    const { email, password, error } = this.state;
+
+    const isInvalid = password === '' || email === '';
+
     return (
       <form onSubmit={this.onSubmit}>
+
+        <button disable={ isInvalid } type='submit'>
+          Sign In
+        </button>
+        { error && <p>{ error.message }</p>}
       </form>
     );
   }
 }
+
+const SignInForm = compose(
+  withRouter,
+  withFirebase
+)(SignInFormBase);
 
 const SignInLink = () => (
   <p>
@@ -38,4 +76,4 @@ const SignInLink = () => (
 );
 
 export default SignIn;
-export { SignInForm, SignInLink };
+export { SignInForm };
