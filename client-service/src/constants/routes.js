db3fd39feece6cb937e5f55af653e8181ba7f76f
rodrigolopezag <rodrigolopezag@gmail.com>
2019-10-19 15:21:56 -0700

    Added home route:

--- client-service/src/constants/routes.js
+++ client-service/src/constants/routes.js
@@ -1,8 +1,9 @@
 export const LANDING = '/';
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
-export const PASSWORD_FORGET = '/psswd-forget';
+export const PASSWORD_FORGET = '/password-forget';
 export const SUBSCRIBE = '/subscribe';
 export const ADMIN_ROUTINES = '/admin-routines';
 export const CHECK_ROUTINES = '/check-routines';
 export const LOG_ROUTINES = '/log-routines';
+export const HOME = '/home';
