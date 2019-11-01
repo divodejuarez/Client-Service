34aceb919abfb3c88132ed44ca91aa99388f1536
divodejuarez <roroaguilera217@hotmail.com>
2019-11-01 00:13:28 -0700

    Made the entry point for the session and authorization components

--- client-service/src/components/Session/index.js
+++ client-service/src/components/Session/index.js
@@ -1,4 +1,5 @@
 import AuthUserContext from './context';
 import withAuthentication from './withAuthenticaton';
+import withAuthorzation from './withAuthorization';
 
-export { AuthUserContext, withAuthentication };
+export { AuthUserContext, withAuthentication, withAuthorization };
